a6d99bc1d06becf807edbe5039133287c439a93d
Michael <rmicu1903@gmail.com>
2023-11-09 19:41:01 -0500

    image links are placed in arrays for display

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,35 @@ import './App.css'
 import Card from './components/card'
 import Scoreboard from './components/scoreboard'
 
+
 function App() {
-  
+
+  let pokeImages = [
+    { id: 0, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" },
+    { id: 1, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png" },
+    { id: 2, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png" },
+    { id: 3, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png" },
+    { id: 4, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg" },
+    { id: 5, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png" },
+    { id: 6, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png" },
+    { id: 7, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png" },
+    { id: 8, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png" },
+    { id: 9, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" },
+    { id: 10, url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/172.png" }
+  ];
+
+
+  const pokeCards = pokeImages.map(poke => <Card pokeImg={poke.url} key={poke.id} />);
+  pokeImages.map(poke =>console.log(poke.url));
 
   return (
     <>
-    <Scoreboard/>
-    <div className='cardContainer'>
-    <Card/>
-    <Card/>
-    <Card/>
-    </div>
+      <Scoreboard />
+      <Card pokeImg={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"}/>
+      <div className='cardContainer'>
+        {pokeCards}
+      </div>
+
     </>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import viteImg from "../assets/vite.png"
+import blastoise from '../assets/pokemonImages/blastoise.png'
 
-function card(pokeImg,pokeName) {
+function card(pokeImg) {
+ 
 
+    //pokeimage needs state ?
+    //const [pokeName, setPokeName] = useState([]);
 
     return (
         <>
             <div  className='card'>
-                <img src={viteImg} className='img'/>
+                <img src={pokeImg} className='img'/>
                 <h1 className='pokeName'>pokemon Name</h1>
             </div>
         </>
